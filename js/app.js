540bc149c45da5b9dd1f13ceb63b2ae434de50b2
raj68518 <raj68518@gmail.com>
2020-03-20 17:48:30 +0530

    second commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const display = document.getElementById('screen');
+const totalIncome = document.getElementsByName('totalIncome');
+const HouseRents = document.getElementsByName('HouseRents')
+const education = document.getElementsByName('education');
+const other = document.getElementsByName('other');
+const totalIncome = document.getElementsByName('totalIncome');
+const calculateBtn = document.getElementById('calculateBtn');
