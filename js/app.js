e9c78269e4b8b03b95560de04131c530437ca36f
raj68518 <raj68518@gmail.com>
2020-03-21 10:29:41 +0530

    restore delete file

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const display = document.getElementById('screen');
+const totalIncome = document.getElementsByName('totalIncome');
+const HouseRents = document.getElementsByName('HouseRents')
+const education = document.getElementsByName('education');
+const other = document.getElementsByName('other');
+const calculateBtn = document.getElementById('calculateBtn');
+
+
+
